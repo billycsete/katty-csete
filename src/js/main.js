37742ed2cb75189a99de2b94f78da9e10fd83629
wscete <wcsete@gmail.com>
2014-12-07 19:10:14 -0800

    added analytics

--- src/js/main.js
+++ src/js/main.js
@@ -9,5 +9,5 @@ var sidebar = Sidebar();
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-43275048-3', 'auto');
+ga('create', 'UA-43275048-4', 'auto');
 ga('send', 'pageview');
