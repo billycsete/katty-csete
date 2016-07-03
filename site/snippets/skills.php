d3ebb89d1dd0516fb8506837068519b25eaf8cdd
wscete <wcsete@gmail.com>
2016-07-02 18:16:02 -0700

    feat(): add kirby cms

---
+++ site/snippets/skills.php
@@ -0,0 +1,6 @@
+<section id="skills" class="skills section">
+	<h2><span><?php echo $data->skills_heading()->html() ?></span></h2>
+	<div class="bullets"><?php echo $data->skills_text()->kirbytext() ?></div>
+	<h2><span><?php echo $data->development_heading()->html() ?></span></h2>
+	<div class="bullets"><?php echo $data->development_text()->kirbytext() ?></div>
+</section><!--/skills-->
