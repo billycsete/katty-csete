9fbbf244261f7d26221f6ec58d654f06f13eae34
Billy Csete <wcsete@apple.com>
2019-01-07 15:33:59 -0800

    fix(): remove second skills section list

--- site/snippets/skills.php
+++ site/snippets/skills.php
@@ -1,6 +1,4 @@
 <section id="skills" class="skills section">
 	<h2><span><?php echo $data->skills_heading()->html() ?></span></h2>
 	<div class="bullets"><?php echo $data->skills_text()->kirbytext() ?></div>
-	<h2><span><?php echo $data->development_heading()->html() ?></span></h2>
-	<div class="bullets"><?php echo $data->development_text()->kirbytext() ?></div>
 </section><!--/skills-->
